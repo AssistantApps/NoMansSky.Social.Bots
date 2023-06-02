e40f9e6910929ed5bf107f69d153985b748bdf4f
Kurt Lourens <hi@kurtlourens.com>
2023-06-02 14:39:53 +0200

    🔖 1.0.23

--- build.ps1
+++ build.ps1
@@ -9,5 +9,8 @@ docker build --no-cache -t nms-social-npc-bots -f Dockerfile --build-arg BUILD_V
 
 # ----------------------- Tag and push to remote -----------------------
 docker tag nms-social-npc-bots registry.local.khaoznet.xyz/nms-social-npc-bots:$version
+docker tag nms-social-npc-bots registry.local.khaoznet.xyz/nms-social-npc-bots:latest
+
 docker push registry.local.khaoznet.xyz/nms-social-npc-bots:$version
+docker push registry.local.khaoznet.xyz/nms-social-npc-bots:latest
 
